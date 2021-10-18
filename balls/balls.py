2d02223d659a18348b43ab328d868c701cea831a
apBUSampK <apbusampk@gmail.com>
2021-10-18 22:41:02 +0300

    Added ammo mechanic (configurable amount of ammo, the game ends when all the ammo ends)
    Created leaderboard system
    
    Minor tweaks

--- balls/balls.py
+++ balls/balls.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.draw import *
 from random import randint, random
 from pygame.font import *
 from math import trunc
@@ -14,6 +13,8 @@ SPAWN_NUMBER = 1
 MAX_BALLS = 8
 MAX_SPEED = 100
 POINT_CONST = 500
+MAX_AMMO = 10
+LEADERBOARD_POS = 10
 
 # game-related global variables
 points = 0
@@ -34,7 +35,8 @@ class Ball:
     """
     A class for storing info about an individual ball, moving and drawing it
     """
-    def __init__(self):
+
+    def __init__(self, surface):
         self.r = randint(10, 100)
         self.x = randint(self.r, 1200 - self.r)
         self.y = randint(self.r, 900 - self.r)
@@ -42,18 +44,19 @@ class Ball:
         self.vy = random() * MAX_SPEED
         self.color = COLORS[randint(0, 5)]
         self.clicked = False
+        self.surface = surface
 
     def draw(self):
-        circle(screen, self.color, (self.x, self.y), self.r)
-        circle(screen, BLACK, (self.x, self.y), self.r, width=2)
+        pygame.draw.circle(self.surface, self.color, (self.x, self.y), self.r)
+        pygame.draw.circle(self.surface, BLACK, (self.x, self.y), self.r, width=2)
 
     def move(self):
-        if self.x - self.r <= 0 or self.x + self.r >= WINDOW_SIZE[0]:
+        if self.x - self.r <= 0 or self.x + self.r >= self.surface.get_width():
             self.vx = -self.vx
-        if self.y - self.r <= 0 or self.y + self.r >= WINDOW_SIZE[1]:
+        if self.y - self.r <= 0 or self.y + self.r >= self.surface.get_height():
             self.vy = -self.vy
-        self.x += self.vx/FPS
-        self.y += self.vy/FPS
+        self.x += self.vx / FPS
+        self.y += self.vy / FPS
 
 
 class BallsList:
@@ -61,11 +64,13 @@ class BallsList:
     A class for organizing info about multiple balls with update() method
     that is called every frame and add_ball() method for creating new balls
     """
-    def __init__(self):
+
+    def __init__(self, surface):
         self.balls_list = []
+        self.surface = surface
 
     def add_ball(self):
-        self.balls_list.append(Ball())
+        self.balls_list.append(Ball(self.surface))
 
     def update(self):
         for ball in self.balls_list:
@@ -83,10 +88,71 @@ def mouse_click(event, ball):
     """
     A method for handling MOUSEBUTTONDOWN events
     """
-    global points
-    if ((event.pos[0] - ball.x)**2 + (event.pos[1] - ball.y)**2)**0.5 <= ball.r:
+    global points, ammo
+    if ((event.pos[0] - ball.x) ** 2 + (event.pos[1] - ball.y) ** 2) ** 0.5 <= ball.r:
         ball.clicked = True
-        points += POINT_CONST/ball.r
+        points += trunc(POINT_CONST / ball.r)
+
+
+def game_over(surface, score):
+    """
+    A method for handling the leaderboard overwrite sequence at the end of the game
+    :param surface: pygame.Surface object to draw in
+    :param score: the score of current player
+    :return: None
+    """
+    lines = 0
+    index = 0
+    _name_ = ""
+    lb = SysFont('freesans', 46)
+    lbB = SysFont('freesans', 128)
+    surface.fill(WHITE)
+    with open("leaderboard", mode='r') as f:
+        prev_board = f.read().split('\n')
+        f.seek(0)
+        for line in f:
+            pygame.draw.rect(surface, BLACK, pygame.Rect(100, 100 + lines * 50, 1000, 50), width=4)
+            surface.blit(lb.render(line.split(' ')[0], True, BLACK), (110, 102 + lines * 50))
+            surface.blit(lb.render(line.split(' ')[1][:-1], True, BLACK), (910, 102 + lines * 50))
+            lines += 1
+            if not index and score > int(line.split(' ')[1]):
+                index = lines
+    pygame.draw.rect(surface, BLACK, pygame.Rect(100, 100 + lines * 50, 1000, 50), width=4)
+    pygame.draw.line(surface, BLACK, (900, 100), (900, 150 + lines * 50), width=4)
+    surface.blit(lb.render(str(score), True, BLACK), (910, 102 + lines * 50))
+    if index or lines < LEADERBOARD_POS:
+        if not index:
+            index = lines
+        saved = False
+        surface.blit(lb.render("Enter your name: " + _name_, True, BLACK),
+                     (110, 102 + lines * 50))
+        pygame.display.update()
+        while not saved:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if pygame.K_SPACE <= event.key <= pygame.K_z:
+                        _name_ += pygame.key.name(event.key)
+                    if event.key == pygame.K_BACKSPACE:
+                        _name_ = _name_[:-1]
+                    pygame.draw.rect(surface, WHITE, pygame.Rect(100, 100 + lines * 50, 800, 50))
+                    pygame.draw.rect(surface, BLACK, pygame.Rect(100, 100 + lines * 50, 800, 50), width=4)
+                    surface.blit(lb.render("Enter your name: " + _name_, True, BLACK),
+                                 (110, 102 + lines * 50))
+                    pygame.display.update()
+                    if event.key == pygame.K_RETURN and len(_name_) > 0:
+                        with open("leaderboard", mode='w') as f:
+                            for i in range(index - 1):
+                                f.write(prev_board[i] + '\n')
+                            f.write(_name_ + ' ' + str(score) + '\n')
+                            for i in range(index - 1, min(lines, LEADERBOARD_POS)):
+                                f.write(prev_board[i] + '\n')
+                        surface.fill(WHITE)
+                        surface.blit(lbB.render("Score saved!", True, BLACK), (250, 300))
+                        pygame.display.update()
+                        saved = True
+    else:
+        surface.blit(lb.render("Your score is not high enough", True, BLACK), (100, 100 + lines * 50))
+        pygame.display.update()
 
 # initialize pygame and screen surface
 pygame.init()
@@ -94,19 +160,21 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.update()
 
 # initialize other variables
-balls = BallsList()
+balls = BallsList(screen)
 text = SysFont('freesans', 128)
 clock = pygame.time.Clock()
 finished = False
 counter = 0
+ammo = MAX_AMMO
 
 # main loop
-while not finished:
+while ammo and not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            ammo -= 1
             for ball in balls.balls_list:
                 mouse_click(event, ball)
     if counter > 0:
@@ -118,8 +186,17 @@ while not finished:
         counter = FRAMES_TO_SPAWN - 1
     screen.fill(WHITE)
     balls.update()
-    screen.blit(text.render(str(trunc(points)), True, BLACK), (0, 0))
+    screen.blit(text.render(str(points), True, BLACK), (0, 0))
+    for x in range(0, ammo * 50, 50):
+        screen.blit(pygame.image.load("shotgun shell.png"), (10 + x, 800))
     pygame.display.update()
 
-  
+game_over(screen, points)
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
 pygame.quit()
